11ed2599b588d6f76b8c698cb221f0a22dae39c0
bforbort <github@k1qv.com>
2021-07-03 21:09:32 -0700

    Fix offset for StealthTouch mount

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1147,7 +1147,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 28, -33, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, 28, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
